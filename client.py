fd68adec0f940a0a2ba2959e387762a9d48b35e3
Isuru Prasad <41295242+isuruprasaddilhara@users.noreply.github.com>
2023-12-04 15:20:41 +0530

    added a progress bar

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 import os 
+import tqdm
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost",888))
+s.connect(("192.168.8.188",888))
 
 def send_file():
     file_name = input("Enter File Name : ")
     file_size = os.path.getsize(file_name)
     s.send(file_name.encode())
     s.send(str(file_size).encode())
+
+    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
 
     with open(file_name,"rb") as file:
         c=0
@@ -18,7 +21,11 @@ def send_file():
                 break
             s.sendall(data)
             c+=len(data)
+            progress.update(1024)
 
-    print("File Transfer completed.")
+    if c==file_size:
+        print("\nFile Transfer completed.")
+    else:
+        print("Error! File Transfer not completed.")
 
 send_file()
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import socket
+import tqdm
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("localhost",888))
+s.bind(("192.168.8.188",888))
 print("[+] Waiting for a connection... ")
 s.listen(5)
-
 def receive_file(client):
     file_name = client.recv(1024).decode()
     file_size = client.recv(1024).decode()
-
-    with open("D:\Education Related\\"+file_name,"wb") as file:
+    
+    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
+    
+    with open("/home/isuru/Desktop/"+file_name,"wb") as file:
         c=0
         while c<=int(file_size):
             data = client.recv(1024)
@@ -17,7 +19,12 @@ def receive_file(client):
                 break
             file.write(data)
             c+=len(data)
-    print("Completed.")
+            progress.update(1024)
+
+    if c == int(file_size):
+        print("File Transfer Completed.")
+    else:
+        print("Error! File Transfer not completed.")
 
 while True:
     client,addr = s.accept()
