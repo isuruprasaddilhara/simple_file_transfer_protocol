15a8ea649f0b8b32ce5ac1910ffafc3fbb06284d
Isuru Prasad <41295242+isuruprasaddilhara@users.noreply.github.com>
2023-12-13 14:43:40 +0530

    first commit

--- client.py
+++ client.py
@@ -1,31 +1,35 @@
-import socket
-import os 
-import tqdm
+def send():
+    import socket
+    import os 
+    import tqdm
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.8.188",888))
+    sender_ip = input("Enter IP address of the machine you want to send data : ")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((sender_ip,888))
+    def send_file():
+        file_name = input("Enter File Name : ")
+        file_size = os.path.getsize(file_name)
+        s.send(file_name.encode())
+        s.send(str(file_size).encode())
 
-def send_file():
-    file_name = input("Enter File Name : ")
-    file_size = os.path.getsize(file_name)
-    s.send(file_name.encode())
-    s.send(str(file_size).encode())
+        progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
 
-    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
+        with open(file_name,"rb") as file:
+            c=0
+            while c<=file_size:
+                data=file.read(1024)
+                if not(data):
+                    break
+                s.sendall(data)
+                c+=len(data)
+                progress.update(1024)
 
-    with open(file_name,"rb") as file:
-        c=0
-        while c<=file_size:
-            data=file.read(1024)
-            if not(data):
-                break
-            s.sendall(data)
-            c+=len(data)
-            progress.update(1024)
-
-    if c==file_size:
-        print("\nFile Transfer completed.")
-    else:
-        print("Error! File Transfer not completed.")
-
-send_file()
+        if c==file_size:
+            print("\nFile Transfer completed.")
+        else:
+            print("Error! File Transfer not completed.")
+        print('')
+        print('----------------------------------------------------------------------------')
+   
+    send_file()
+send()
--- server.py
+++ server.py
@@ -1,34 +1,41 @@
-import socket
-import tqdm
+def receive():
+    import socket
+    import tqdm
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("192.168.8.188",888))
-print("[+] Waiting for a connection... ")
-s.listen(5)
-def receive_file(client):
-    file_name = client.recv(1024).decode()
-    file_size = client.recv(1024).decode()
+    my_address = socket.gethostbyname(socket.gethostname());print(my_address)
     
-    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
-    
-    with open("/home/isuru/Desktop/"+file_name,"wb") as file:
-        c=0
-        while c<=int(file_size):
-            data = client.recv(1024)
-            if not(data):
-                break
-            file.write(data)
-            c+=len(data)
-            progress.update(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((my_address,888))
+    print("[+] Waiting for a connection... ")
+    s.listen(5)
+    def receive_file(client):
+        file_name = client.recv(1024).decode()
+        file_size = client.recv(1024).decode()
+        
+        progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,total=int(file_size))
+        
+        with open("D:\Education Related\\"+file_name,"wb") as file:
+            c=0
+            while c<=int(file_size):
+                data = client.recv(1024)
+                if not(data):
+                    break
+                file.write(data)
+                c+=len(data)
+                progress.update(1024)
 
-    if c == int(file_size):
-        print("File Transfer Completed.")
-    else:
-        print("Error! File Transfer not completed.")
+        if c == int(file_size):
+            print("File Transfer Completed.")
+        else:
+            print("Error! File Transfer not completed.")
 
-while True:
-    client,addr = s.accept()
-    print(f"We got a connection from {addr}")
-    receive_file(client)
-    client.close()
+    while True:
+        client,addr = s.accept()
+        print(f"We got a connection from {addr}")
+        receive_file(client)
+        client.close()
 
+        print('')
+        print('----------------------------------------------------------------------------')
+
+receive()
